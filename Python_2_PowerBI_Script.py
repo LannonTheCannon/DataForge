a00a424ca53921285ffaba76012ede674091f4c1
LannonTheCannon <49844413+LannonTheCannon@users.noreply.github.com>
2024-09-13 12:44:24 -0700

    Create Python_2_PowerBI_Script.py

---
+++ Python_2_PowerBI_Script.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from pathlib import Path
+
+def load_file(path):
+    with open(path, 'rb') as f:
+        return pd.read_pickle(f)
+
+folder = r"C:\Users\16269\DataspellProjects\DataForge\data"
+all_datasets = [load_file(file) for file in Path(folder).iterdir()]
+df = pd.concat(all_datasets)
+
+print(df.shape)
+print(df.dtypes)
